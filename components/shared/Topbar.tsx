e503ae52b61c7b21489d1807dea1a19677586324
Michele Picozzi <michele.picozzi@hotmail.it>
2023-08-10 11:47:35 +0800

    topbar and rightsidebar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -2,6 +2,7 @@ import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { dark } from "@clerk/themes";
 
 export default function Topbar() {
     return (
@@ -23,6 +24,7 @@ export default function Topbar() {
                 </div>
                 <OrganizationSwitcher
                     appearance={{
+                        baseTheme: dark,
                         elements: {
                             organizationSwitcherTrigger: "py-2 px-4",
                         },
