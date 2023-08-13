6a22a1abb95a890a85c5acd5846c6ad486b256db
Michele Picozzi <michele.picozzi@hotmail.it>
2023-08-13 10:20:04 +0800

    it starts the onboarding form checking

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,6 +1,14 @@
 "use client";
+
 import React from "react";
-import { Form } from "../ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserValidation } from "@/lib/validations/user";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import * as z from "zod";
+import Image from "next/image";
 
 interface Props {
     user: {
@@ -15,5 +23,48 @@ interface Props {
 }
 
 export default function AccountProfile({ user, btnTitle }: Props) {
-    return <Form></Form>;
+    const form = useForm({
+        resolver: zodResolver(UserValidation),
+        defaultValues: {
+            profile_photo: "",
+            name: "",
+            username: "",
+            bio: "",
+        },
+    });
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof UserValidation>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values);
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col justify-start gap-10">
+                <FormField
+                    control={form.control}
+                    name="profile_photo"
+                    render={({ field }) => (
+                        <FormItem className="flex items-center gap-4">
+                            <FormLabel className="account-form_image-label">
+                                {field.value ? (
+                                    <Image src={field.value} alt="profile photo" width={96} height={96} priority className="rounded-full object-contain" />
+                                ) : (
+                                    <Image src="/assets/profile.svg" alt="profile photo" width={24} height={24} className="object-contain" />
+                                )}
+                            </FormLabel>
+                            <FormControl>
+                                <Input placeholder="shadcn" {...field} />
+                            </FormControl>
+                            <FormDescription>This is your public display name.</FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    );
 }
