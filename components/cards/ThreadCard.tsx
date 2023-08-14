e0ca7f62d3725a8ede9f95bb1bd4fb57ba5a4eda
Michele Picozzi <michele.picozzi@hotmail.it>
2023-08-14 22:04:43 +0800

    it styles the thread post

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface Props {
@@ -25,8 +27,44 @@ export default function ThreadCard({
     comments,
 }: Props) {
     return (
-        <article>
-            <h2 className="text-small-regular text-light-2">{content}</h2>
+        <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+            <div className="flex items-start justify-between">
+                <div className="flex w-full flex-1 flex-row gap-4">
+                    <div className="flex flex-col items-center">
+                        <Link
+                            className="overflow-hidden relative h-11 w-11"
+                            href={`/profile/${author.id}`}
+                        >
+                            <Image
+                                src={author.image}
+                                alt="Profile Image"
+                                fill
+                                className="cursor-pointer rounded-full"
+                            />
+                        </Link>
+                        <div className="thread-card_bar" />
+                    </div>
+                    <div className="flex w-full flex-col">
+                        <Link className="w-fit" href={`/profile/${author.id}`}>
+                            <h4 className="cursor-pointer text-base-semibold text-light-1">
+                                {author.name}
+                            </h4>
+                        </Link>
+                        <p className="mt-2 text-small-regular text-light-2">{content}</p>\
+                        <div className="mt-5 flex flex-col gap-3">
+                            <div className="flex gap-3.5">
+                                <Image
+                                    src="/assets/heart-gray.svg"
+                                    alt="heart"
+                                    width={24}
+                                    height={24}
+                                    className="cursor-pointer object-contain"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </article>
     );
 }
