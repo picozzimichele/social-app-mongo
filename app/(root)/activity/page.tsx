6985550ebc1f174e3992b3ccf07d2a53b19cbf80
Michele Picozzi <michele.picozzi@hotmail.it>
2023-08-16 17:27:39 +0800

    it does the activity page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -2,6 +2,7 @@ import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { fetchUser, getActivity } from "@/lib/actions/user.actions";
 import Link from "next/link";
+import Image from "next/image";
 
 export default async function Page() {
     const user = await currentUser();
@@ -23,7 +24,31 @@ export default async function Page() {
         <section>
             <h1 className="head-text mb-10">Activity</h1>
             <section className="mt-10 flex flex-col gap-5">
-                {activity.length > 0 ? <>{activity.map((activity: any) => <Link key={activity._id}></Link>))}</> : <p className="no-result">No Activity yet</p>}
+                {activity.length > 0 ? (
+                    <>
+                        {activity.map((activity: any) => (
+                            <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                                <article className="activity-card">
+                                    <Image
+                                        src={activity.author.image}
+                                        alt="user_logo"
+                                        width={20}
+                                        height={20}
+                                        className="rounded-full object-cover"
+                                    />
+                                    <p className="!text-small-regular text-light-1">
+                                        <span className="mr-1 text-primary-500">
+                                            {activity.author.name}
+                                        </span>{" "}
+                                        replied to your thread
+                                    </p>
+                                </article>
+                            </Link>
+                        ))}
+                    </>
+                ) : (
+                    <p className="!text-base-regular text-light-3">No activity yet</p>
+                )}
             </section>
         </section>
     );
